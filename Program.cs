34ac56ebff904f559d3638fa84633888ba644de5
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-02 13:18:30 +0530

    Fibonacci Series

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LogicalProgramming;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Calling FibonacciSeries");
+FibonacciSeries fibonacciSeries = new FibonacciSeries();
+fibonacciSeries.Fibonacci();
+
